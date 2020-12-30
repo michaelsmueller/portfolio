3baf8694edb803efc517b9ac28d75aab954a4065
Michael Mueller <hello@michaelmueller.dev>
2020-12-30 17:18:35 +0100

    yarn upgrade --latest, Emotion breaking changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-import { Global } from '@emotion/core';
+import { Global } from '@emotion/react';
 import globalStyles from 'styles/global';
 import typeStyles from 'styles/typography';
 import Footer from 'components/Footer';
